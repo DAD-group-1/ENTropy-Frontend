7f5074472b59446cfbe8b9d0a6c8b7d214245507
Anthony <schneppanthony@gmail.com>
2026-05-13 16:21:16 +0200

    Login page implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './page-modules/home/home-page';
+import { LoginPage } from './page-modules/login/login-page';
 
-export const routes: Routes = [{
-  path: 'home',
-  component: HomePage,
-}];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePage,
+  },
+  {
+    path: 'login',
+    component: LoginPage,
+  },
+];
