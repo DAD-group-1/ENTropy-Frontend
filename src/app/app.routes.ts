7047b79cb6a5d7f55983f713e44e2f8ec8002e98
Anthony <schneppanthony@gmail.com>
2026-05-23 17:43:41 +0200

    Updated the routes + the guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,49 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth/auth-guard';
 import { HomePage } from './page-modules/home/home-page';
 import { LoginPage } from './page-modules/login/login-page';
 import { CalendarPage } from './page-modules/calendar/calendar-page';
+import { GradesPage } from './page-modules/grades/grades-page';
+import { AbsencesPage } from './page-modules/absences/absences-page';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    component: HomePage,
-  },
   {
     path: 'login',
     component: LoginPage,
   },
+
   {
-    path: 'calendar',
-    component: CalendarPage,
+    path: '',
+    // TODO: Activate authGuard when auth service is implemented
+    // canMatch: [authGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomePage,
+      },
+      {
+        path: 'calendar',
+        component: CalendarPage,
+        // TODO: See how to implement permissions check
+        // data: { roles: ['USER', 'ADMIN'] },
+      },
+      {
+        path: 'grades',
+        component: GradesPage,
+        // TODO: See how to implement permissions check
+        // data: { roles: ['USER', 'ADMIN'] },
+      },
+      {
+        path: 'absences',
+        component: AbsencesPage,
+        // TODO: See how to implement permissions check
+        // data: { roles: ['USER', 'ADMIN'] },
+      },
+    ],
   },
 ];
