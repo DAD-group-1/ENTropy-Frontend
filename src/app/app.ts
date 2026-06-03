c3a0bdb002f2c51ca2e9f6ed74f6ab85ecb3d7da
Anthony <schneppanthony@gmail.com>
2026-06-03 16:44:23 +0200

    Remove useless import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { PrimeNG } from 'primeng/config';
 import { AuthService } from './shared-modules/service/auth.service';
 import { Header } from './shared-modules/layout/header/header';
@@ -13,7 +13,6 @@ import { LayoutService } from './shared-modules/service/layout.service';
   styleUrl: './app.css',
 })
 export class App implements OnInit {
-  router = inject(Router);
   primeng = inject(PrimeNG);
   authService = inject(AuthService);
   layoutService = inject(LayoutService);
