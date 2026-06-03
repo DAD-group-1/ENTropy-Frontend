c73a1bafa32fb751f4452fb0e36b4a7be665dbbd
Anthony <schneppanthony@gmail.com>
2026-06-03 21:20:21 +0200

    Added optimized image

--- src/app/shared-modules/layout/header/header.ts
+++ src/app/shared-modules/layout/header/header.ts
@@ -3,10 +3,11 @@ import { NgmMotionDirective } from '@scripttype/ng-motion';
 import { LayoutService } from '../../service/layout.service';
 import { RouterLink } from '@angular/router';
 import { Notification } from '../notification/notification';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [NgmMotionDirective, RouterLink, Notification],
+  imports: [NgmMotionDirective, RouterLink, Notification, NgOptimizedImage],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
