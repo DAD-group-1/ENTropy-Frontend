a5ae9be2a593edb1cc95c73f80b157e61320c4e1
Anthony <schneppanthony@gmail.com>
2026-06-08 17:21:42 +0200

    Changed the resources input style

--- src/app/page-modules/resources/resources-page.ts
+++ src/app/page-modules/resources/resources-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { InputText } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { InputGroupAddon } from 'primeng/inputgroupaddon';
+import { InputGroup } from 'primeng/inputgroup';
 
 @Component({
   selector: 'app-resources-page',
-  imports: [InputText, ButtonModule],
+  imports: [InputText, ButtonModule, InputGroupAddon, InputGroup],
   templateUrl: './resources-page.html',
   styleUrl: './resources-page.css',
 })
