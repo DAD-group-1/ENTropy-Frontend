237dcab38df92d6544f8e92ae09546df45b281ff
Anthony <schneppanthony@gmail.com>
2026-06-10 17:19:18 +0200

    Base calendar working

--- src/app/page-modules/calendar/calendar-page.ts
+++ src/app/page-modules/calendar/calendar-page.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
   CalendarEvent,
   ENTCalendar,
 } from '../../shared-modules/app-common/entcalendar/entcalendar';
+import {
+  ScheduleFindByDateRangeDefaultResponse,
+  ScheduleResponseDto,
+  ScheduleService,
+  StudentService,
+} from '../../core/data-services';
+import { FrontAuthService, Roles } from '../../shared-modules/service/front-auth.service';
+import { FrontNavigationService } from '../../shared-modules/service/front-navigation.service';
+import { Observable, of, switchMap } from 'rxjs';
+import { displayName } from '../../shared-modules/utils';
 
 @Component({
   selector: 'app-calendar-page',
@@ -11,33 +21,92 @@ import {
   styleUrl: './calendar-page.css',
 })
 export class CalendarPage {
+  private readonly frontNavigationService = inject(FrontNavigationService);
+  private readonly frontAuthService = inject(FrontAuthService);
+  private readonly schedulesService = inject(ScheduleService);
+  private readonly studentService = inject(StudentService);
+
   //TODO: Get it from the Authorization service
   public isInstructor = false;
+  public loading = signal(true);
 
-  private TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
+  public calendarEvents = signal<CalendarEvent[]>([]);
 
-  //TODO: Do a query to the backend
-  public calendarEvents: CalendarEvent[] = [
-    {
-      id: '1',
-      title: 'title',
-      start: this.TODAY_STR,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      room: '101',
-      tutor: 'MR. Tutor TEST',
-    },
-    {
-      id: '2',
-      title: 'Timed event',
-      start: this.TODAY_STR + 'T08:00:00',
-      end: this.TODAY_STR + 'T09:00:00',
-    },
-    {
-      id: '3',
-      title: 'Timed event',
-      start: this.TODAY_STR + 'T12:00:00',
-      end: this.TODAY_STR + 'T15:00:00',
-    },
-  ];
+  loadSchedule(startDate: Date, endDate: Date) {
+    const role = this.frontAuthService.tokenPersonalizedData?.role;
+
+    if (!role) {
+      this.frontNavigationService.navigate('/forbidden');
+      return;
+    }
+
+    const startDateRequest = startDate.toISOString();
+    const endDateRequest = endDate.toISOString();
+    const dateObj = {
+      startDate: startDateRequest,
+      endDate: endDateRequest,
+    };
+
+    let request$: Observable<ScheduleFindByDateRangeDefaultResponse> | undefined;
+
+    this.loading.set(true);
+
+    switch (role) {
+      case Roles.STUDENT: {
+        request$ = this.studentService.studentFindOne({ id: this.frontAuthService.userId! }).pipe(
+          switchMap((result) => {
+            return this.schedulesService.scheduleFindByDateRange({
+              programId: String(result.data?.program_id),
+              ...dateObj,
+            });
+          }),
+        );
+        break;
+      }
+
+      case Roles.INSTRUCTOR: {
+        request$ = this.schedulesService.scheduleFindByInstructorId({
+          instructorId: this.frontAuthService.userId!,
+          ...dateObj,
+        });
+        break;
+      }
+
+      default: {
+        request$ = of({
+          success: true,
+          data: [],
+        } as ScheduleFindByDateRangeDefaultResponse);
+        break;
+      }
+    }
+
+    request$?.subscribe({
+      next: (result: ScheduleFindByDateRangeDefaultResponse) => {
+        console.log(result);
+        this.calendarEvents.set((result.data ?? []).map(this.scheduleToCalendarEvent));
+        this.loading.set(false);
+      },
+      error: (err) => {
+        console.error(err);
+        this.frontNavigationService.navigate('error');
+      },
+    });
+  }
+
+  onRangeChange = (start: Date, end: Date) => {
+    this.loadSchedule(start, end);
+  };
+
+  scheduleToCalendarEvent(schedule: ScheduleResponseDto): CalendarEvent {
+    return {
+      id: String(schedule.id),
+      title: schedule.course.name,
+      start: schedule.start_date,
+      end: schedule.end_date,
+      room: schedule.room.name,
+      instructor: displayName(schedule.instructor.user.first_name, schedule.instructor.user.last_name),
+    };
+  }
 }
+
